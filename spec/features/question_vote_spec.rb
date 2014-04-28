40c47236bf64c499baea88197f50b5612ab1de75
Paolo Payumo <paolo.payumo@yahoo.com>
2014-04-27 17:48:05 -0700

    fixed all vote tests

--- spec/features/question_vote_spec.rb
+++ spec/features/question_vote_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 feature "Voting on question" do
-  let!(:question) { FactoryGirl.create :question }
   let!(:user) { FactoryGirl.create :user}
+  let!(:question) { FactoryGirl.create :question }
   before :each do
     page.set_rack_session(:user_id => user.id)
-    visit root_path
-    click_on question.title
+    visit question_path(question)
   end
 
   scenario "a user can vote on questions" do
@@ -14,7 +13,7 @@ feature "Voting on question" do
     expect(page).to have_content('Not Boss')
   end
 
-  scenario "a user can vote a question up", js: true do
+  scenario "a user can vote a question up" do
     click_button "Boss"
     expect(page).to have_content(question.vote_count)
   end
